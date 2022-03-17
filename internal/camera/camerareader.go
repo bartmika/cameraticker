ec8709ed219e13132ef1e71e363c6eb7987c7dbe
Bartlomiej Mika <bmika@icloud.com>
2022-03-17 01:38:40 -0400

    Added code for reading previously saved images and have the data accessible via Golangs `image.Image` struct.

--- internal/camera/camerareader.go
+++ internal/camera/camerareader.go
@@ -1,5 +1,8 @@
 package camera
 
+import "image"
+
 type CameraStillReader interface {
 	Snapshot() error
+	GetLatestImage() (image.Image, error)
 }
--- internal/camera/libcamerastill.go
+++ internal/camera/libcamerastill.go
@@ -3,6 +3,10 @@ package camera
 import (
 	"errors"
 	"fmt"
+	"image"
+	_ "image/jpeg" // Add support for jpeg format.
+	_ "image/png"  // Add support for png format.
+	"os"
 	"os/exec"
 	"strconv"
 	"time"
@@ -10,14 +14,17 @@ import (
 
 // LibCameraStill represents the Golang wrapper over the `libcamera-still` command line application that is found in the Raspberry Pi OS.
 type LibCameraStill struct {
-	workingDir string
-	format     string
-	fileExt    string
-	width      int
-	height     int
+	workingDir     string
+	format         string
+	fileExt        string
+	width          int
+	height         int
+	latestFilePath string
 }
 
+// NewLibCameraStill creates an instance of our LibCameraStill struct.
 func NewLibCameraStill(width int, height int, format string, workingDirectoryAbsoluteFilePath string) (*LibCameraStill, error) {
+	// See available formats via https://www.raspberrypi.com/documentation/accessories/camera.html#encoders
 	fileFormat := map[string]string{
 		"png":    "png",
 		"bmp":    "bmp",
@@ -38,9 +45,9 @@ func NewLibCameraStill(width int, height int, format string, workingDirectoryAbs
 
 // Snapshot will take a snapshot with the Raspberry Pi camera module and save it to the specified file. This function is essentially a wrapper function over the `libcamera-still` command.
 func (cam *LibCameraStill) Snapshot() error {
-	// Generate the new filename for our camera still. See available formats via https://www.raspberrypi.com/documentation/accessories/camera.html#encoders
-
+	// Generate the new filename for our camera still and save.
 	filename := cam.workingDir + "/" + strconv.Itoa(int(time.Now().Unix())) + "." + cam.fileExt
+	cam.latestFilePath = filename
 
 	// DEVELOPERS NOTE:
 	// We are using the included `libcamera-still` command to handle taking a
@@ -65,4 +72,25 @@ func (cam *LibCameraStill) Snapshot() error {
 
 	fmt.Println(string(stdout))
 	return nil
+}
+
+// GetLatestImage returns the latest image.Image data captured previously and saved to disk by the Raspberry Pi camera.
+func (cam *LibCameraStill) GetLatestImage() (image.Image, error) {
+	if cam.latestFilePath == "" {
+		return nil, errors.New("no snapshot taken yet ... please wait")
+	}
+	return getImageFromFilePath(cam.latestFilePath)
+}
+
+func getImageFromFilePath(filePath string) (image.Image, error) {
+	// Special thanks:
+	// https://stackoverflow.com/a/49595208
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	image, _, err := image.Decode(f)
+	return image, err
 }
